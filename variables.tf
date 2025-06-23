a5323dece0cc3ce330a34f1a1010ebe7f1e87e7d
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-23 11:21:25 -0600

    v3

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type = string
+}
+
+
+## commit
