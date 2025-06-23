54687e4b0190923a78a9630893e9866fd7c59fa8
Diego Enrique Islas Cuervo <dislas@caabsa.com.mx>
2025-06-23 13:03:52 -0600

    se configura v4

--- main.tf
+++ main.tf
@@ -117,6 +117,27 @@ resource "azurerm_network_interface" "nic4" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+resource "azurerm_windows_virtual_machine" "vm4" {
+  name                = "vm-basic4"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic4.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
 
 
 
