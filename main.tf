8799ac1e15b38fbf04016f6f0c0d04c80525feaf
Diego Enrique <dislas@caabsa.com.mx>
2025-06-23 18:58:34 -0600

    se añade vm 04

--- main.tf
+++ main.tf
@@ -193,4 +193,30 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 
+resource "azurerm_windows_virtual_machine" "vm04" {
+  name                = "vm-basic04"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
